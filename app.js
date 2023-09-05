b2742f383d2422ab2185d2bbf933bd76a7620474
Richu-Gokul <113789844+Richu-Gokul@users.noreply.github.com>
2023-09-05 17:16:11 +0530

    Add files via upload
    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// // Get references to the page and modal elements
+// const page2 = document.getElementById("page2");
+// const modal = document.getElementById("myModal");
+// const showModalBtn = document.getElementById("showModalBtn");
+// const closeBtn = document.getElementsByClassName("close")[0];
+
+// // Show the modal when the second page is visited
+// page2.addEventListener("DOMContentLoaded", () => {
+//   modal.style.display = "block";
+// });
+
+// // Close the modal when the close button is clicked
+// closeBtn.addEventListener("click", () => {
+//   modal.style.display = "none";
+// });
